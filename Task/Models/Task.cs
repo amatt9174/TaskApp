9fd17fc8606dc62643054dff057ff41de7fa4147
amatt9174@gmail.com <amatt9174@gmail.com>
2023-07-08 12:57:49 -0500

    Add project files.

---
+++ Task/Models/Task.cs
@@ -0,0 +1,14 @@
+﻿namespace Task.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? CreatedOn { get; set; }
+        public string? Comments { get; set; }
+        public string? AssignedTo { get; set; }
+        public string? Priority { get; set; }
+        public string? Status { get; set; }
+        public string? StatusChangedOn { get; set; }
+    }
+}
